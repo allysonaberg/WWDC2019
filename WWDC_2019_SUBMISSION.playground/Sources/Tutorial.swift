1fac9772b7433026c7e963d4ad13ea875be3a87b
Allyson Aberg <allya7897@gmail.com>
2019-03-21 20:17:08 -0400

    semi-working tutorial

--- WWDC_2019_SUBMISSION.playground/Sources/Tutorial.swift
+++ WWDC_2019_SUBMISSION.playground/Sources/Tutorial.swift
@@ -4,33 +4,55 @@ class Tutorial: SKNode {
   
   var text: SKLabelNode
   var continueButton: SKLabelNode
+  var page: Int = 0
+  //todo: make play button a lazy var
+  var playButton: SKLabelNode
   var root: GameScene
   
   let continueButtonName = "continue"
+  let playButtonName = "play"
+  
+  //todo: build out pages as separate classes... also put the text in a dic
+  let pageText = [
+  "This is a game about BLEH",
+  "bla bla bloo",
+  "haha haha haha",
+  "AHHHHHHHHHHH",
+  ]
   
   //todo: do I need to add text and continueButton as children?
   init(_ root: GameScene, size: CGSize) {
     self.root = root
     
-    self.text = SKLabelNode(text: "this is tutorial text")
+    self.page = 0
+
+    self.text = SKLabelNode(text: pageText[page])
     self.text.color = redColor
     
     self.continueButton = SKLabelNode(text: "CONTINUE")
     self.continueButton.color = redColor
     self.continueButton.name = continueButtonName
     
+    self.playButton = SKLabelNode(text: "PLAY")
+    self.playButton.color = redColor
+    self.playButton.name = playButtonName
+    
     super.init()
 
-    self.addChild(text)
-    
+    self.text.position = CGPoint(x: self.position.x, y: self.position.y)
+    self.continueButton.position = CGPoint(x: self.position.x, y: self.position.y - 200)
+    self.playButton.position = self.continueButton.position
+    self.playButton.isHidden = true
+    self.playButton.isUserInteractionEnabled = false
 
+    self.addChild(text)
     self.addChild(continueButton)
-    
+    self.addChild(playButton)
     
     let overlay = SKSpriteNode()
     overlay.size = size
-    overlay.color = UIColor.blue
-    overlay.alpha = 0.4
+    overlay.color = blackColor
+    overlay.alpha = 0.9
     self.addChild(overlay)
   }
   
@@ -46,16 +68,31 @@ class Tutorial: SKNode {
       for node in nodes
       {
         if node.name == continueButtonName {
+          nextPage()
+        } else if node.name == playButtonName {
           startPlaying()
         }
       }
     }
   }
   
+  private func nextPage() {
+    page+=1
+    if (page < 3) {
+      self.text.text = pageText[page]
+    } else {
+      self.continueButton.isHidden = true
+      self.continueButton.isUserInteractionEnabled = false
+      self.playButton.isHidden = false
+      self.playButton.isUserInteractionEnabled = true
+    }
+  }
+  
+  
   private func startPlaying() {
     print("STARTING")
-    root.hasShownTutorial = true
     self.removeFromParent()
+    root.hasShownTutorial = true
   }
 }
 
