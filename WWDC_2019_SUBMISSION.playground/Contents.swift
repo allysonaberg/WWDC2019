46406a42084b02f7f6bcd0796b20a78008e6622c
Allyson Aberg <allya7897@gmail.com>
2019-03-23 12:19:08 -0400

    tile update

--- WWDC_2019_SUBMISSION.playground/Contents.swift
+++ WWDC_2019_SUBMISSION.playground/Contents.swift
@@ -10,7 +10,7 @@ let viewSize = CGSize(width: 1024, height: 768)
 let sceneView = SKView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: viewSize))
 sceneView.ignoresSiblingOrder = true
 sceneView.showsNodeCount = true
-sceneView.showsPhysics = true
+
 let menuScene = MenuScene(size: viewSize)
 menuScene.scaleMode = .aspectFill
 sceneView.presentScene(menuScene)
