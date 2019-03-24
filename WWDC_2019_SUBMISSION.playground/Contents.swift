8761e5895fbb32f6d2255818b8174121b84ba794
Allyson Aberg <allya7897@gmail.com>
2019-03-24 00:32:40 -0400

    okok good for tn

--- WWDC_2019_SUBMISSION.playground/Contents.swift
+++ WWDC_2019_SUBMISSION.playground/Contents.swift
@@ -2,9 +2,7 @@
 import PlaygroundSupport
 import SpriteKit
 
-
-//TODO: this should probably store both the menu and game scene and switch between them
-//rather than reinitializing
+//TODO: remove skip button
 let viewSize = CGSize(width: 1024, height: 768)
 
 let sceneView = SKView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: viewSize))
